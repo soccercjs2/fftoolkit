82f82ead90812d515e53b6adb449b4742fa669d5
Collin <Collin@STERANKASURFACE>
2018-07-09 19:50:31 -0700

    changing header home link

--- fftoolkit.Web/Controllers/TradeController.cs
+++ fftoolkit.Web/Controllers/TradeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using fftoolkit.DB.Context;
+using fftoolkit.DB.Model;
+using fftoolkit.Logic.Managers;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +12,24 @@ namespace fftoolkit.Controllers
 {
     public class TradeController : Controller
     {
+        private DataContext _context;
+
+        public TradeController()
+        {
+            _context = new DataContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            UserManager userManager = new UserManager(_context);
+            User user = userManager.GetCurrentUser(User.Identity.GetUserId());
+
+            if (user == null)
+            {
+                throw new Exception("No user found;");
+            }
+
+            return View(user.Leagues);
         }
 
         public ActionResult League()
